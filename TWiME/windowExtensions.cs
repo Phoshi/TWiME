d8333663ee90f237516f31b5330b068907b0bd11
Phoshi <AHPhoshi@gmail.com>
2011-07-31 17:06:48 +0100

    Added window extensions

---
+++ TWiME/windowExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TWiME {
+    public static class windowExtensions { //Why extensions? Because I want to keep the Window class standalone
+        public static Monitor Monitor(this Window window) {
+            foreach (Monitor monitor in Manager.monitors) {
+                if ((from screen in monitor.screens select screen.windows).SelectMany(wnd => wnd).Contains(window)) {
+                    return monitor;
+                }
+            }
+            return null;
+        }
+    }
+}
