14147c2e75eed07e82d18e0e69fb28edff59b6de
Phoshi <AHPhoshi@gmail.com>
2012-03-11 13:55:40 +0000

    Added string extensions

---
+++ Extensions/stringExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Extensions {
+    public static class stringExtensions {
+        public static int Width(this string str, Font withFont) {
+            int widasdth = TextRenderer.MeasureText(str, withFont).Width;
+            return width;
+        }
+    }
+}
