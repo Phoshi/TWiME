cdcc0bf0298f12690c69697dc76a780ff84a1f56
Phoshi <AHPhoshi@gmail.com>
2012-03-11 17:52:42 +0000

    Added support for tiling "styles" per-window, making TWiME a true hybrid WM (Emulator)

---
+++ TWiME/WindowTilingType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TWiME {
+    public enum WindowTilingType {
+        Normal = 0,
+        FullTag = 1,
+        FullScreen = 2,
+        Floating = 3,
+    }
+}
